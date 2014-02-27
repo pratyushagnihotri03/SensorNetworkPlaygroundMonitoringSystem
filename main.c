0d901800229d885d634ccb99fc8937177446b745
pratyush <pratyushagnihotri03@gmail.com>
2014-02-28 00:04:17 +0100

    main.c and setting.h with actuators command

--- main.c
+++ main.c
@@ -15,12 +15,25 @@ struct my_packet{
 };
 
 enum {
+//TEMP
   COMMAND_TYPE_TEMP_LOW,
   COMMAND_TYPE_TEMP_OK,
-  COMMAND_TYPE_CO2_HIGH,
+  COMMAND_TYPE_TEMP_HIGH,
+//CO2
+  COMMAND_TYPE_CO2_LOW,
   COMMAND_TYPE_CO2_OK,
+  COMMAND_TYPE_CO2_HIGH,
+//LIGHT
   COMMAND_TYPE_LIGHT_LOW,
-  COMMAND_TYPE_LIGHT_OK
+  COMMAND_TYPE_LIGHT_OK,
+//HUMID
+  COMMAND_TYPE_HUMID_LOW,
+  COMMAND_TYPE_HUMID_OK,
+  COMMAND_TYPE_HUMID_HIGH,
+//MOSITURE
+  COMMAND_TYPE_MOIS_LOW,
+  COMMAND_TYPE_MOIS_OK,
+  COMMAND_TYPE_MOIS_HIGH
 };
 
 #define MAX_RETRANSMISSIONS 4
@@ -33,25 +46,103 @@ recv_runicast(struct runicast_conn *c, const rimeaddr_t *from, uint8_t seqno)
 {
   struct my_packet * p;
   p=(struct my_packet *)packetbuf_dataptr();
-  
+//------------------ Temperature--------------------
   if(p->type == COMMAND_TYPE_TEMP_LOW) {
     printf("TEMP LOW! received from %d.%d\n",
-           from->u8[0], from->u8[1]); }
+           from->u8[0], from->u8[1]);
+	//Turning on the HEATER.
+   	 printf("PG:HEAT ON\n");  
+	 }
   else if(p->type == COMMAND_TYPE_TEMP_OK) {
     printf("TEMP OK! received from %d.%d\n",
-           from->u8[0], from->u8[1]); }
+           from->u8[0], from->u8[1]); 
+	//Turning off the Fan and Heater.
+   	 printf("PG:FAN OFF\n");
+	 printf("PG:HEAT OFF\n");  
+	}
+  else if(p->type == COMMAND_TYPE_TEMP_HIGH) {
+    printf("TEMP HIGH! received from %d.%d\n",
+           from->u8[0], from->u8[1]);
+	 //Turning on the Fan.
+   	 printf("PG:FAN ON\n");  
+	}
+//------------------- CO2---------------------------
+  
   if(p->type == COMMAND_TYPE_CO2_HIGH) {
     printf("CO2 HIGH! received from %d.%d\n",
-           from->u8[0], from->u8[1]); }
-  if(p->type == COMMAND_TYPE_CO2_OK) {
+           from->u8[0], from->u8[1]); 
+    	//Turning on the Fan.
+   	 printf("PG:FAN ON\n");  
+	}
+  else if(p->type == COMMAND_TYPE_CO2_OK) {
     printf("CO2 OK! received from %d.%d\n",
-           from->u8[0], from->u8[1]); }
+           from->u8[0], from->u8[1]); 
+	//Turning off the Fan.
+   	 printf("PG:FAN OFF\n"); 
+	}
+  else if(p->type == COMMAND_TYPE_CO2_LOW) {
+    printf("CO2 LOW! received from %d.%d\n",
+           from->u8[0], from->u8[1]); 
+	//GIVE CO2.
+   	 printf(" ! GIVE CO2 !\n"); 
+	}
+//------------------- LIGHT--------------------------- 
   if(p->type == COMMAND_TYPE_LIGHT_LOW) {
     printf("LIGHT LOW! received from %d.%d\n",
-           from->u8[0], from->u8[1]); }
-  if(p->type == COMMAND_TYPE_LIGHT_OK) {
+           from->u8[0], from->u8[1]); 
+ 	//Turning on the lamp.
+   	 printf("PG:LIGHT ON\n");
+	}
+  else if(p->type == COMMAND_TYPE_LIGHT_OK) {
     printf("LIGHT OK! received from %d.%d\n",
-           from->u8[0], from->u8[1]); }
+           from->u8[0], from->u8[1]);
+ 	//Turning off the lamp.
+   	 printf("PG:LIGHT OFF\n");
+	}
+
+//-------------------HUMIDITY---------------------------
+
+  if(p->type == COMMAND_TYPE_HUMID_LOW) {
+    printf("LIGHT LOW! received from %d.%d\n",
+           from->u8[0], from->u8[1]); 
+ 	//Turning on the Heater.
+   	 printf("PG:HEAT ON\n");
+	}
+  else if(p->type == COMMAND_TYPE_HUMID_OK) {
+    printf("TEMP OK! received from %d.%d\n",
+           from->u8[0], from->u8[1]); 
+	//Turning off the Fan and Heater.
+   	 printf("PG:FAN OFF\n");
+	 printf("PG:HEAT OFF\n");  
+	}
+  else if(p->type == COMMAND_TYPE_HUMID_HIGH) {
+    printf("LIGHT OK! received from %d.%d\n",
+           from->u8[0], from->u8[1]);
+ 	//Turning on the Fan.
+   	 printf("PG:FAN ON\n"); 
+       }
+
+//-------------------SOIL MOISTURE-----------------------
+
+  if(p->type == COMMAND_TYPE_MOIS_LOW) {
+    printf("LIGHT LOW! received from %d.%d\n",
+           from->u8[0], from->u8[1]); 
+ 	//Give water to the plant.
+   	 printf("! GIVE WATER !\n");
+	}
+  else if(p->type == COMMAND_TYPE_MOIS_OK) {
+    printf("LIGHT OK! received from %d.%d\n",
+           from->u8[0], from->u8[1]);
+ 	//Turning off the heater.
+   	 printf("PG:HEAT OFF\n"); 
+	}
+  else if(p->type == COMMAND_TYPE_MOIS_HIGH) {
+    printf("LIGHT OK! received from %d.%d\n",
+           from->u8[0], from->u8[1]);
+ 	//Turning on the heater.
+   	 printf("PG:HEAT ON\n");
+        }
+  
 }
 static void
 sent_runicast(struct runicast_conn *c, const rimeaddr_t *to, uint8_t retransmissions)
@@ -76,7 +167,7 @@ static struct runicast_conn uc;
 PROCESS(main_process, "main");
 AUTOSTART_PROCESSES(&main_process);
 
-static struct etimer et,settling_time;
+static struct etimer et;
 static uint16_t my_id;
 PROCESS_THREAD(main_process, ev, data)
 {
@@ -89,8 +180,6 @@ PROCESS_THREAD(main_process, ev, data)
 
 	PROCESS_EXITHANDLER(runicast_close(&uc);)
 	PROCESS_BEGIN();
-	etimer_set(&settling_time, CLOCK_SECOND * 120);
-	PROCESS_WAIT_UNTIL(etimer_expired(&settling_time));
 
 	my_id = rimeaddr_node_addr.u8[1] * 256 + rimeaddr_node_addr.u8[0];
 	addr.u8[0] = ID_SINK % 256;
@@ -102,7 +191,7 @@ PROCESS_THREAD(main_process, ev, data)
 		SENSORS_ACTIVATE(light_sensor);
 		SENSORS_ACTIVATE(sht11_sensor);
 	}
-   else if (my_id == ID_CO2)
+       else if (my_id == ID_CO2)
 		SENSORS_ACTIVATE(ds1000_sensor);
 
 	runicast_open(&uc, 140, &runicast_callbacks);
@@ -127,38 +216,97 @@ PROCESS_THREAD(main_process, ev, data)
 		if (my_id == ID_MOIST) {
 			value1 = vh400.value(ADC0);
 			print_values("Moisture :", value1);
+
+//------------------------------MOISTURE Actuators------------------------ //
+			if(value1 < THRESHOLD_MOIS_LOW ){
+				p.type = COMMAND_TYPE_MOIS_LOW;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			else if (value1 > THRESHOLD_MOIS_HIGH ){
+				p.type = COMMAND_TYPE_MOIS_HIGH;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			else if (value1 <=THRESHOLD_MOIS_HIGH   && value1 >= THRESHOLD_MOIS_LOW){
+				p.type = COMMAND_TYPE_MOIS_OK;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
 		}
 		else if (my_id == ID_LIGHT) {
 			value1 = light_sensor.value(LIGHT_SENSOR_TOTAL_SOLAR);
 			value2 = sht11_sensor.value(SHT11_SENSOR_HUMIDITY);
 			value3 = sht11_sensor.value(SHT11_SENSOR_TEMP);
-			temp_val= ((-39.8) + (value3 * 0.01));
-			double humidity_val= (-2.0468 + (0.0367 * value2) + ( -1.5955E-6 * (pow(value2,2)) ) );
-			if (humidity_val>100){
-				humi_val = 100;
-				print_values("Humidity_Value :", humi_val );
-			}
-			else{
-				humi_val = humidity_val;
-				print_values("Humidity_Value :", humi_val );
-			}
+			temp_val= ((0.01 * value3 -32)* .55);
+			double humidity_val= (-4.0 + 405.0*value2/10000);
+			humi_val = (int) humidity_val;
+		    
 			print_values("Light_Sensor_ADC :", value1);
 			print_values("Humidity_ADC :", value2);
+			print_values("Humidity_Value :", humi_val );
 			print_values("Temperature_ADC :", value3);
 			print_values("Temperature °C :", temp_val);
+
+//------------------------------Light Actuators------------------------ //
+
+			if(value1 < THRESHOLD_LIGHT ){
+				p.type = COMMAND_TYPE_LIGHT_LOW;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			else {
+				p.type = COMMAND_TYPE_LIGHT_OK;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+//------------------------------Temperature Actuators------------------------ //
+			if(value3 < THRESHOLD_TEMP_LOW ){
+				p.type = COMMAND_TYPE_TEMP_LOW;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			else if (value3 > THRESHOLD_TEMP_HIGH ){
+				p.type = COMMAND_TYPE_TEMP_HIGH;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			else if (value3 <=THRESHOLD_TEMP_HIGH   && value3 >= THRESHOLD_TEMP_LOW ){
+				p.type = COMMAND_TYPE_TEMP_OK;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+//------------------------------HUMIDITY Actuators------------------------ //
+			if(value2 < THRESHOLD_HUMID_LOW ){
+				p.type = COMMAND_TYPE_HUMID_LOW;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			else if (value2 > THRESHOLD_HUMID_HIGH ){
+				p.type = COMMAND_TYPE_HUMID_HIGH;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			else if (value2 <=THRESHOLD_HUMID_HIGH   && value2 >= THRESHOLD_HUMID_LOW){
+				p.type = COMMAND_TYPE_HUMID_OK;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			
+			
 		}
 		else if (my_id == ID_CO2) {
 
 			adc = ds1000_sensor.value(SENSOR_CO2);
   			raw_voltage = (double)(adc/4096.0)*2.5; 
-			value1 = ((double)(ds1000_sensor.value(SENSOR_CO2)/4096.0)*2.5 * 1000) - 200;
+			value1 = ((double)(adc/4096.0)*2.5 * 1000) - 200;
 			printf("CO2_ADC: ", adc);
 			printf("CO2_Raw_Voltage :", raw_voltage);  
 			print_values("CO2 ppm:", value1);
-			if (value1 > THRESHOLD_CO2)
-				;//send CO2_HIGH to sink
+//------------------------------cO2 Actuators------------------------ //
+			if(value1 < THRESHOLD_CO2_LOW ){
+				p.type = COMMAND_TYPE_CO2_LOW;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			else if (value1 > THRESHOLD_CO2_HIGH ){
+				p.type = COMMAND_TYPE_CO2_HIGH;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+			else if (value1 <=THRESHOLD_CO2_HIGH   && value1 >= THRESHOLD_CO2_LOW){
+				p.type = COMMAND_TYPE_CO2_OK;
+				runicast_send(&uc, &addr, MAX_RETRANSMISSIONS);
+			}
+				
 		}
-
 
 		packetbuf_copyfrom(&p,sizeof(struct my_packet));
 
--- settings.h
+++ settings.h
@@ -2,12 +2,22 @@
 #define __SETTINGS_H__
 
 /* set node IDs here */
-#define ID_SINK 3001
-#define ID_CO2 3002
-#define ID_MOIST 3003
-#define ID_LIGHT 3009
+#define ID_SINK 1
+#define ID_MOIST 2
+#define ID_LIGHT 3
+#define ID_CO2 4
 
-/* set threshold values here */
-#define THRESHOLD_CO2 0
+
+/* PePeromia Plant : set threshold values here */
+#define THRESHOLD_CO2_HIGH 81
+#define THRESHOLD_CO2_LOW 58
+#define THRESHOLD_LIGHT 640
+#define THRESHOLD_TEMP_HIGH 28 
+#define THRESHOLD_TEMP_LOW 24
+      // Sample value 
+#define THRESHOLD_MOIS_LOW 1765 
+#define THRESHOLD_MOIS_HIGH 1950
+#define THRESHOLD_HUMID_LOW 20
+#define THRESHOLD_HUMID_HIGH 40
 
 #endif /* __SETTINGS_H__ */
