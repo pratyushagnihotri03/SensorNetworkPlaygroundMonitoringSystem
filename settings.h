65bda546f907ec68776128cf116ed95418de9f4f
Rick Nitsche <ricknitsche@gmx.de>
2014-02-28 13:54:51 +0100

    ifdefs for 2 LEFT/RIGHT plant

--- settings.h
+++ settings.h
@@ -1,23 +1,46 @@
 #ifndef __SETTINGS_H__
 #define __SETTINGS_H__
 
+#define RIGHT //peperomia
+//#define LEFT //kalanchoe
+
 /* set node IDs here */
-#define ID_SINK 3001
-#define ID_MOIST 3003
 #define ID_LIGHT 3009
 #define ID_CO2 3002
 
+#ifdef RIGHT //peperomia
+#define ID_SINK 3001
+#define ID_MOIST 3003
+#endif //RIGHT
 
-/* PePeromia Plant : set threshold values here */
+#ifdef LEFT //kalanchoe
+#define ID_SINK 3008
+#define ID_MOIST 3007
+#endif //LEFT
+
+/* set threshold values here */
+#ifdef RIGHT //peperomia
 #define THRESHOLD_CO2_HIGH 81
 #define THRESHOLD_CO2_LOW 58
 #define THRESHOLD_LIGHT 640
 #define THRESHOLD_TEMP_HIGH 28 
 #define THRESHOLD_TEMP_LOW 24
-      // Sample value 
 #define THRESHOLD_MOIS_LOW 1765 
 #define THRESHOLD_MOIS_HIGH 1950
 #define THRESHOLD_HUMID_LOW 20
 #define THRESHOLD_HUMID_HIGH 40
+#endif //RIGHT
+
+#ifdef RIGHT //kalanchoe
+#define THRESHOLD_CO2_HIGH 81
+#define THRESHOLD_CO2_LOW 58
+#define THRESHOLD_LIGHT 640
+#define THRESHOLD_TEMP_HIGH 28 
+#define THRESHOLD_TEMP_LOW 24
+#define THRESHOLD_MOIS_LOW 1765 
+#define THRESHOLD_MOIS_HIGH 1950
+#define THRESHOLD_HUMID_LOW 20
+#define THRESHOLD_HUMID_HIGH 40
+#endif //RIGHT
 
 #endif /* __SETTINGS_H__ */
