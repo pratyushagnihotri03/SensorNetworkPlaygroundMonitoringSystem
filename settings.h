6fab161b7efaea21faa25302d22c4a1d4348bc94
Rick Nitsche <ricknitsche@gmx.de>
2014-03-13 14:55:36 +0100

    fixed errors

--- settings.h
+++ settings.h
@@ -1,15 +1,10 @@
 #ifndef __SETTINGS_H__
 #define __SETTINGS_H__
 
-#define REAL //peperomia and kalanchoe
-//#define COOJA
-//#define TEST
-
 #define RIGHT 0
 #define LEFT 1
 
 /* set node IDs here */
-#ifdef REAL 
 // RIGHT = peperomia
 #define ID_SINK_R 3001
 #define ID_MOIST_R 3003
@@ -20,22 +15,7 @@
 
 #define ID_LIGHT 3009
 #define ID_CO2 3002
-#endif //REAL
 
-#ifdef COOJA
-#define ID_SINK 1
-#define ID_MOIST 2
-#define ID_LIGHT 3
-#define ID_CO2 4
-#endif //COOJA
-
-#ifdef TEST
-#define ID_SINK 1
-#define ID_MOIST 2
-#define ID_LIGHT 22998
-#define ID_CO2 3
-#define COOJA
-#endif //TEST
 
 /* set threshold values here */
 //RIGHT = peperomia
@@ -48,29 +28,6 @@ const uint16_t THRESHOLD_MOIS_LOW[] = { 1774, 1502 };
 const uint16_t THRESHOLD_MOIS_HIGH[] = { 2428, 1917 };
 const uint16_t THRESHOLD_HUMID_LOW[] = { 20, 20 };
 const uint16_t THRESHOLD_HUMID_HIGH[] = { 40, 40 };
-
-/*
-#define THRESHOLD_CO2_HIGH_R 950
-#define THRESHOLD_CO2_LOW_R 40
-#define THRESHOLD_LIGHT_R 680
-#define THRESHOLD_TEMP_HIGH_R 28 
-#define THRESHOLD_TEMP_LOW_R 24
-#define THRESHOLD_MOIS_LOW_R 1774 
-#define THRESHOLD_MOIS_HIGH_R 2428
-#define THRESHOLD_HUMID_LOW_R 20
-#define THRESHOLD_HUMID_HIGH_R 40
-
-//LEFT = kalanchoe
-#define THRESHOLD_CO2_HIGH_R 950
-#define THRESHOLD_CO2_LOW_R 40
-#define THRESHOLD_LIGHT_R 1200
-#define THRESHOLD_TEMP_HIGH_R 29 
-#define THRESHOLD_TEMP_LOW_R 12.7
-#define THRESHOLD_MOIS_LOW_R 1502 
-#define THRESHOLD_MOIS_HIGH_R 1917
-#define THRESHOLD_HUMID_LOW_R 20
-#define THRESHOLD_HUMID_HIGH_R 40
-*/
 
 //offset values for debouncing
 #define OFFSET_MOIST 100
