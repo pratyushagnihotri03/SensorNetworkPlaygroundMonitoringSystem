85f835104ab8b0c31e6a42a044d5091bf7c534ff
Rick Nitsche <ricknitsche@gmx.de>
2014-03-13 14:21:01 +0100

    co2.c

--- co2.c
+++ co2.c
@@ -4,7 +4,7 @@ uint8_t measure_co2()
 {
 	static uint16_t raw;
 	static double co2;
-	static uint8_t i, state_co2 = COMMAND_TYPE_CO2_OK;
+	static uint8_t i, state_co2[2] = {0,0};
 
 	raw = ds1000_sensor.value(SENSOR_CO2);
 	co2 = ((double)(raw/4096.0)*2.5 * 1000) - 200;
@@ -14,18 +14,18 @@ uint8_t measure_co2()
 
 //------------------------------cO2 Actuators------------------------ //
 	for (i = 0; i < 2; i++) {
-		if(state_co2 != COMMAND_TYPE_CO2_LOW && co2 < THRESHOLD_CO2_LOW ){
-			return state_co2 = COMMAND_TYPE_CO2_LOW;
+		if(state_co2[i] != COMMAND_TYPE_CO2_LOW && co2 < THRESHOLD_CO2_LOW[i] ){
+			state_co2[i] = COMMAND_TYPE_CO2_LOW;
 		}
-		else if (state_co2 != COMMAND_TYPE_CO2_HIGH && co2 > THRESHOLD_CO2_HIGH ){
-			return state_co2 = COMMAND_TYPE_CO2_HIGH;
+		else if (state_co2[i] != COMMAND_TYPE_CO2_HIGH && co2 > THRESHOLD_CO2_HIGH[i] ){
+			state_co2[i] = COMMAND_TYPE_CO2_HIGH;
 		}
-		else if (state_co2 != COMMAND_TYPE_CO2_OK 
-			&& co2 <=THRESHOLD_CO2_HIGH - OFFSET_CO2 
-			&& co2 >= THRESHOLD_CO2_LOW + OFFSET_CO2){
-			return state_co2 = COMMAND_TYPE_CO2_OK;
+		else if (state_co2[i] != COMMAND_TYPE_CO2_OK 
+			&& co2 <=THRESHOLD_CO2_HIGH[i] - OFFSET_CO2 
+			&& co2 >= THRESHOLD_CO2_LOW[i] + OFFSET_CO2){
+			state_co2[i] = COMMAND_TYPE_CO2_OK;
 		}
 	}
 
-	return 0;
+	return state;
 }
